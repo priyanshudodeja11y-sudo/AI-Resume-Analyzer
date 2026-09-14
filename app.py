e63ce4ef325de8108ec36f8b76f564df7b6204df
priyanshudodeja11y-sudo <priyanshudodeja11y@gmail.com>
2026-09-15 00:04:47 +0530

    add job skill visualization

--- app.py
+++ app.py
@@ -123,6 +123,18 @@ if resume_file and job_description:
         with right:
             st.markdown("### ⚠️ Skills to Improve")
             st.write(", ".join(missing_skills) if missing_skills else "No major predefined skill gaps detected.")
+            st.markdown("### 📊 Job Skill Match Visualization")
+
+            skill_chart_data = {
+            "Skill": job_skills,
+             "Match": [1 if skill in resume_skills else 0 for skill in job_skills]
+}
+
+              st.bar_chart(
+              skill_chart_data,
+              x="Skill",
+              y="Match"
+)
 
         st.markdown("### Extracted Resume Text")
         st.text_area("Text", resume_text[:8000], height=250)
